456becb72586b1e72cc6feb5109963f79b2ff013
Casey M <caseym@gmail.com>
2017-03-01 18:11:20 -0500

    deleted pycache

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from main import views as main_views
 from contact import views as contact_views
+from payments import views as payment_views
 
 admin.autodiscover()
 
@@ -24,5 +25,11 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', main_views.index, name='home'),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
-    url(r'^contact/', contact_views.contact, name='contact')
+    url(r'^contact/', contact_views.contact, name='contact'),
+
+    # # user registration/authentication
+    # url(r'^sign_in$', payment_views.sign_in, name='sign_in'),
+    # url(r'^sign_out$', payment_views.sign_out, name='sign_out'),
+    # url(r'^register$', payment_views.register, name='register'),
+    # url(r'^edit$', payment_views.edit, name='edit'),
 ]
